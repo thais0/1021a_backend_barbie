99342543c3411a9718ce49b28f6ed455bfd7874c
thais0 <thais.santos13@estudante.ifms.edu.br>
2023-10-25 08:03:28 -0400

    25/10

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,15 @@ app.get('/', (req, res) => {
     res.send('Olá, mundo!');
 });
 
+app.post('/filmes', (req, res) => {
+    const filme = {
+        titulo: 'Vingadores',
+        descricao: 'Filme dos Vingadores',
+        foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
+    }
+    res.status(201).send(filme)
+});
+
 // Inicia o servidor
 app.listen(3000, () => {
     console.log('Servidor iniciado na porta 3000');
--- test/index.test.ts
+++ test/index.test.ts
@@ -19,4 +19,22 @@ describe('Cadastro Filme', () => {
         expect(resposta.status).toBe(201)
         expect(resposta.data).toEqual(filme)
     })
+    it('deve listar um filme cadastrar', async () => {
+      const filme = {
+          titulo: 'Vingadores',
+          descricao: 'Filme dos Vingadores',
+          foto: 'https://live.staticflickr.com/7270/6976087418_59719341f5_b.jpg',
+      }
+      const resposta = await axios.post('http://localhost:3000/filmes', filme)
+
+      //1-buscar o filme cadastrado
+      const listaFilmes = await axios.get('http://localhost:3000/filmes')
+      const filmeCadastrado = listaFilmes.data
+
+      //2-verificar se o filme devolvido é igual ao que nós cadastramos
+      expect(filmeCadastrado[0]).toEqual(filme)
+
+      //3-verificar se o tamanho da lista é igual a 1
+      expect(listaFilmes.data.length).toBe(1)
+  })
 })
